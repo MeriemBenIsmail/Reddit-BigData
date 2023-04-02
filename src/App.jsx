6c57edc574736151fa716f4d2959ea22997f3689
meriem <meriembenismail@insat.u-carthage.tn>
2023-04-02 22:23:13 +0100

    batch view container

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import LiveView from "./pages/LiveView";
 import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import BatchView from "./pages/BatchView";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/live-view" element={<LiveView />} />
+        <Route exact path="/batch-view" element={<BatchView />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/BatchViewContainer/BatchViewContainer.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import classes from './BatchViewContainer.module.css'
+
+export const BatchViewContainer = () => {
+  return (
+    <div>BatchViewContainer</div>
+  )
+}
