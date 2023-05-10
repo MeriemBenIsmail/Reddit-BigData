bbe8aadcbcf13b2c8de02b58e769748a3afbd2c5
Taher Jerbi <jerbi.taherr@gmail.com>
2023-05-10 16:32:13 +0100

    feat: connect to ws server

--- src/components/LiveViewContainer/LiveViewContainer.jsx
+++ src/components/LiveViewContainer/LiveViewContainer.jsx
@@ -3,40 +3,44 @@ import { useEffect, useState } from "react";
 import { MOCK_COMMENTS } from "../../mockComments";
 import LiveViewItem from "../LiveViewItem/LiveViewItem";
 
+// type Sentiment = "Positive" | "Negative" | "Neutral" | "Unknown";
+// type Comment = {
+//   comment_id: string;
+//   parent_id: string;
+//   body: string;
+//   subreddit: string;
+//   timestamp: string;
+//   sentiment: Sentiment;
+// }
+
+
+// connect to websocket server
+const socket = new WebSocket("ws://localhost:4000");
+
 const LiveViewContainer = () => {
+  
   const [comments, setComments] = useState([]);
 
-  const getNewValue = () => {
-    return MOCK_COMMENTS[Math.floor(Math.random() * MOCK_COMMENTS.length)];
-  };
-
-  const getNewValues = () => {
-    const newValueArray = [];
-    for (let i = 0; i < 6; i++) {
-      newValueArray.push(getNewValue());
-    }
-    return newValueArray;
-  };
-
-  const updateComments = () => {
-    const updatedComments = comments.slice(6);
-    updatedComments.push(...getNewValues());
-    setComments(updatedComments);
-  };
-
   useEffect(() => {
-    if (!comments.length) {
-      updateComments();
-    }
-    setTimeout(() => {
-      updateComments();
-    }, 2000);
-  }, [comments]);
+    socket.onopen = () => {
+      console.log("connected to websocket server");
+    };
+    socket.onmessage = (message) => {
+      console.log("🚀 ~ file: LiveViewContainer.jsx:29 ~ useEffect ~ message:", message)
+      try {
+        const comments = JSON.parse(message.data);
+        setComments(comments);
+      }
+      catch (e) {
+        console.log("🚀 ~ file: LiveViewContainer.jsx:29 ~ useEffect ~ e:", e)
+      }
+    }}, []);
+  
 
   return (
     <div className={classes.liveViewContainer}>
       {comments.slice(0, 6).map((comment, index) => {
-        return <LiveViewItem text={comment.body} sentiment={comment.sentiment} key={index} />;
+        return <LiveViewItem text={comment.body} sentiment={comment.sentiment} key={comment.comment_id} />;
       })}
     </div>
   );
--- src/components/LiveViewItem/LiveViewItem.jsx
+++ src/components/LiveViewItem/LiveViewItem.jsx
@@ -2,18 +2,35 @@ import classes from "./LiveViewItem.module.css";
 import { getColor } from "../LiveViewContainer/LiveView.color";
 import negative from "../../pages/negative.png";
 import positive from "../../pages/positive.png";
+
+// type Sentiment = "Positive" | "Negative" | "Neutral" | "Unknown";
+
+function sentimentToColor(sentiment) {
+  switch (sentiment) {
+    case "Positive":
+      return "lightgreen";
+    case "Negative":
+      return "#ffd1dc";
+    case "Neutral":
+      return "#ffe4e1";
+    default:
+      return "lightgray";
+  }
+}
+
 const LiveViewItem = ({ text, sentiment }) => {
   return (
     <div
       className={classes.liveViewItem}
-      style={{ backgroundColor: getColor() }}
+      style={{ backgroundColor: sentimentToColor(sentiment) }}
     >
       <div className={classes.text}>{text}</div>
       <img
         className={classes.sentiment}
-        src={sentiment === 1 ? positive : negative}
+        src={sentiment === "Positive" ? positive : negative}
         alt="sentiment"
       />
+      <p className={classes.sentimentText}>{sentiment}</p>
     </div>
   );
 };
